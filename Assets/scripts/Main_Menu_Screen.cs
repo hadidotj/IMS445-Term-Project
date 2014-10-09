d53636f6a4b94b820e427dba39cc262079ba4a03
Aaron Baker <bakeraj4@miamioh.edu>
2014-10-09 09:16:59 -0400

    Combined Tyler's stuff with mine.
    I also made a build of the current game as build2.0.

--- Assets/scripts/Main_Menu_Screen.cs
+++ Assets/scripts/Main_Menu_Screen.cs
@@ -3,14 +3,13 @@ using System.Collections;
 
 public class Main_Menu_Screen : MonoBehaviour {
 	public Texture background;
-	public int[] scenes;
 	void OnGUI(){
 		GUI.DrawTexture (new Rect(0,0,Screen.width,Screen.height),background);
 		if(GUI.Button(new Rect(Screen.width/4-90,Screen.height/2+175,180,65),"Options")){
-			Application.LoadLevel(scenes[0]);
+			Application.LoadLevel("options_menu");
 		}
 		else if(GUI.Button(new Rect(Screen.width/2-90,Screen.height/2+175,180,65),"Play")){
-			Application.LoadLevel(scenes[1]);
+			Application.LoadLevel("getting_ready_to_play");
 		}
 		else if(GUI.Button(new Rect(Screen.width*3/4-90,Screen.height/2+175,180,65),"Exit")){
 			Application.Quit();
--- Assets/scripts/Options_Menu_Screen.cs
+++ Assets/scripts/Options_Menu_Screen.cs
@@ -7,7 +7,7 @@ public class Options_Menu_Screen : MonoBehaviour {
 	void OnGUI(){
 		GUI.DrawTexture (new Rect(0,0,Screen.width,Screen.height),background);
 		if(GUI.Button(new Rect(Screen.width/2-90,Screen.height/2+175,180,65),"Return")){
-			Application.LoadLevel(1);
+			Application.LoadLevel("main_menu");
 		}
 		// TODO have options stuff
 	}
