c63106926a02062c0490d6b46e6c68eea0eb4cbe
Tyler Hadidon <hadidotj@miamioh.edu>
2014-10-21 08:12:54 +0100

    Adding new buttons to menus. Join has field for entering IP and connect button. Create has a create button. Logic not yet implemented

--- Assets/scripts/Menus/CreateMenu.cs
+++ Assets/scripts/Menus/CreateMenu.cs
@@ -5,24 +5,38 @@ public class CreateMenu : AbstractMenu {
 
 	public Texture2D header;
 	public Texture2D back;
+	public Texture2D create;
 
 	private int currentHoveredOver = -1;
 
 	public override void Draw(){
+		Vector3 mouse = Input.mousePosition;
+		mouse.y = Screen.height-mouse.y;
+
 		// Draw header
 		GUI.DrawTexture(new Rect(Screen.width/2.0f - header.width/2.0f, 10, header.width, header.height), header);
 
+		// Go Button
+		GUI.backgroundColor = Color.blue;
+		Rect connectButtonRect = new Rect(Screen.width-create.width-10.0f, Screen.height-create.height-10.0f, create.width, create.height);
+		if(GUI.Button (connectButtonRect, create)){
+			Debug.Log("Create new server!");
+		} else if(currentHoveredOver != 0 && connectButtonRect.Contains(mouse)) {
+			currentHoveredOver = 0;
+			ButtonHover();
+		} else if(currentHoveredOver == 0 && !connectButtonRect.Contains(mouse)) {
+			currentHoveredOver = -1;
+		}
+		
 		// Back Button
 		GUI.backgroundColor = Color.red;
 		Rect backButtonRect = new Rect(10.0f, Screen.height-back.height-10.0f, back.width, back.height);
-		Vector3 mouse = Input.mousePosition;
-		mouse.y = Screen.height-mouse.y;
 		if(GUI.Button (backButtonRect, back)){
 			ButtonPressed("MainMenu");
-		} else if(currentHoveredOver != 0 && backButtonRect.Contains(mouse)) {
-			currentHoveredOver = 0;
+		} else if(currentHoveredOver != 1 && backButtonRect.Contains(mouse)) {
+			currentHoveredOver = 1;
 			ButtonHover();
-		} else if(currentHoveredOver == 0 && !backButtonRect.Contains(mouse)) {
+		} else if(currentHoveredOver == 1 && !backButtonRect.Contains(mouse)) {
 			currentHoveredOver = -1;
 		}
 	}
--- Assets/scripts/Menus/JoinMenu.cs
+++ Assets/scripts/Menus/JoinMenu.cs
@@ -5,24 +5,42 @@ public class JoinMenu : AbstractMenu {
 
 	public Texture2D header;
 	public Texture2D back;
+	public Texture2D connect;
 	
 	private int currentHoveredOver = -1;
+	private string joinIP = "Server IP";
 	
 	public override void Draw(){
+		Vector3 mouse = Input.mousePosition;
+		mouse.y = Screen.height-mouse.y;
+		
 		// Draw header
-		GUI.DrawTexture(new Rect(Screen.width/2.0f - header.width/2.0f, 10, header.width, header.height), header);
+		GUI.Label(new Rect(Screen.width/2.0f - header.width/2.0f, 10, header.width, header.height), header);
+
+		// IP Input
+		joinIP = GUI.TextField(new Rect(Screen.width/2.0f-200, header.height+50, 400, 25), joinIP);
+
+		// Go Button
+		GUI.backgroundColor = Color.green;
+		Rect connectButtonRect = new Rect(Screen.width-connect.width-10.0f, Screen.height-connect.height-10.0f, connect.width, connect.height);
+		if(GUI.Button (connectButtonRect, connect)){
+			Debug.Log("Connect TO Server!");
+		} else if(currentHoveredOver != 0 && connectButtonRect.Contains(mouse)) {
+			currentHoveredOver = 0;
+			ButtonHover();
+		} else if(currentHoveredOver == 0 && !connectButtonRect.Contains(mouse)) {
+			currentHoveredOver = -1;
+		}
 		
 		// Back Button
 		GUI.backgroundColor = Color.red;
 		Rect backButtonRect = new Rect(10.0f, Screen.height-back.height-10.0f, back.width, back.height);
-		Vector3 mouse = Input.mousePosition;
-		mouse.y = Screen.height-mouse.y;
 		if(GUI.Button (backButtonRect, back)){
 			ButtonPressed("MainMenu");
-		} else if(currentHoveredOver != 0 && backButtonRect.Contains(mouse)) {
-			currentHoveredOver = 0;
+		} else if(currentHoveredOver != 1 && backButtonRect.Contains(mouse)) {
+			currentHoveredOver = 1;
 			ButtonHover();
-		} else if(currentHoveredOver == 0 && !backButtonRect.Contains(mouse)) {
+		} else if(currentHoveredOver == 1 && !backButtonRect.Contains(mouse)) {
 			currentHoveredOver = -1;
 		}
 	}
