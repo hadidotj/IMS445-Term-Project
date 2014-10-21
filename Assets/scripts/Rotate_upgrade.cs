ed015be5a87cef71f9e11bf022d3aaa1d01a86c5
Aaron Baker <bakeraj4@miamioh.edu>
2014-10-20 23:48:40 -0400

    I updated the fire power up.
    The behaviour isn't included yet. It's just a prefab with collider,
    appearence,, and a rigid body.

---
+++ Assets/scripts/Rotate_upgrade.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate_upgrade : MonoBehaviour {
+	
+	private float t = 0.0f;
+	void Update() {
+		t += Time.deltaTime % 360.0f;
+		gameObject.transform.Rotate(new Vector3(0, 1, 0), t);
+	}
+}
