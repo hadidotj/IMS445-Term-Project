abe70a120e4bfff9ac4e7cf088e10303fcfeb711
yakkelsj <yakkelsj@muohio.edu>
2014-11-20 01:40:53 -0500

    TDM started
    Started working on Team Death Match and setting up a game mode manager
    so that we can choose game type. Not fully implemented though.

---
+++ Assets/scripts/GameModeManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameModeManager : MonoBehaviour {
+	
+	private int gameMode;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameMode == 0) {
+			// Play Search and Destroy
+		} else if(gameMode == 1) {
+			// Play Team Death Match
+		} else {
+			// Play Free For All
+		}
+
+	}
+}
